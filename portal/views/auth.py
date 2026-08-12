6eb115b43c0e6446e9903754bd66998f39568e07
JosueBurela <jburela1@gmail.com>
2026-08-11 18:08:10 -0600

    fix(auth): importar Q en portal/views/auth.py

--- portal/views/auth.py
+++ portal/views/auth.py
@@ -10,6 +10,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import check_password
 from django.contrib import messages
+from django.db.models import Q
 from portal.models import PersonalAdministrativo, Ciudadano
 
 import random
