ae660f97a5ba060e8cd28b4b5a281796e877e1c3
JosueBurela <jburela1@gmail.com>
2026-09-04 16:48:13 -0600

    fix(core): establecer LOGIN_URL en settings para evitar redireccion 404 al acceder a areas protegidas

--- core/settings.py
+++ core/settings.py
@@ -176,3 +176,4 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Protección Civil Medellín <soporte@medellin.gob.mx>')
 EMAIL_TIMEOUT = 5
+LOGIN_URL = 'login_unificado'
